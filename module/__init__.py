0865861cccf69cfc9097460c14cc2218d33ee7ed
Jeremy Hyde <jeremyrhyde@Jeremys-MacBook-Pro-2.local>
2023-09-19 17:14:10 -0400

    change import in init

--- module/__init__.py
+++ module/__init__.py
@@ -1,8 +1,8 @@
 from viam.components.generic import Generic
 from viam.resource.registry import Registry, ResourceCreatorRegistration
-from .chatgpt import MyResource
+from .chatgpt import MyChatGPTInstance
 
 
 Registry.register_resource_creator(Generic.SUBTYPE,
-                                   MyResource.MODEL,
-                                   ResourceCreatorRegistration(MyResource.new))
+                                   MyChatGPTInstance.MODEL,
+                                   ResourceCreatorRegistration(MyChatGPTInstance.new))
